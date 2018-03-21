ade264535e27c96b36dac507fb24b642ebd43ef9
quobod <quobod@gmail.com>
2018-03-21 15:03:46 -0400

    Modified app file

--- app.js
+++ app.js
@@ -46,5 +46,5 @@ app.use(function(err, req, res, next) {
 app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function(){
-	console.log('Server started on port ' + app.get('port'));
+	console.log(`Server started on port ${app.get('port')}`);
 });
